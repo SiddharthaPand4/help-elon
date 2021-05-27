c656cddd37efaf03b1075c92ba6ea2395f799dc0
Siddharth Pandey <siddhartha@tdgpartner.info>
2021-05-27 20:38:30 +0530

    first test and mint and burn functionality

--- test/HelpElonToken.js
+++ test/HelpElonToken.js
@@ -1,18 +1,38 @@
 const helpElonContract = artifacts.require("HelpElonToken");
+var expect = require('chai').expect;
 
-contract("HelpElonToken", (accounts) => {
+contract("HelpElonToken", (accounts) => {	
 	let [alice, bob] = accounts;
-	
 	let contractInstance;
 
 	beforeEach(async () => {
 		contractInstance = await helpElonContract.new({from: alice});
 	});
 
-	it("should be able to mint tokens", async () => {
-		await contractInstance.mint(alice, 1000, {from: alice});
+	context("testing basic coin attributes", async () => {
+	        const name = "HELPELON";
+		const symbol = "$HELPELON";
+		const decimals = 16;
+		it("testing coin name", async ()=> {
+			const result = await contractInstance.name.call();
+			expect(result).to.be.a("string");
+			expect(result).to.equal(name);
+		})
+		it("testing coin symbol", async ()=> {
+			const result = await contractInstance.symbol.call();
+			expect(result).to.be.a("string");
+			expect(result).to.equal(symbol);
+		})
+		it("testing decimal places", async ()=> {
+			const result = await contractInstance.decimals.call();
+			expect(result).to.be.a("number");
+			assert.equal(result.valueOf(), decimals);
+		})
+	})
+	it("should be able to mint specified number of tokens", async () => {
+		const mintCoins = 1000;
+		await contractInstance.mint(alice, mintCoins, {from: alice});
 		const result = await contractInstance.totalSupply.call();
-		//console.log(result.toNumber());
-		assert.equal(result.valueOf(), 1000);
+		assert.equal(result.valueOf(), mintCoins);
 	})
 })
